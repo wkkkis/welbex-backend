6cd2c55397e4812f36357f9f86f9046a94b7dcae
Issak <aizekyuy@yandex.ru>
2022-10-23 21:35:57 +0600

    some fixes

--- db.js
+++ db.js
@@ -1,12 +1,16 @@
 const Pool = require("pg").Pool;
 const options = require("./utils/constants");
 
-const pool = new Pool({
-    connectionString:
-        "postgres://rfaxkorcnzfkmx:f8da7956a6a5fc97d9cdbc7f1719bd951a15421f1fd25b8cb7870ac3a5bda9cb@ec2-54-147-36-107.compute-1.amazonaws.com:5432/d9v3bk99vn40l3",
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
+const usuRightOptions = process.env.PRODUCTION
+    ? {
+          connectionString:
+              "postgres://rfaxkorcnzfkmx:f8da7956a6a5fc97d9cdbc7f1719bd951a15421f1fd25b8cb7870ac3a5bda9cb@ec2-54-147-36-107.compute-1.amazonaws.com:5432/d9v3bk99vn40l3",
+          ssl: {
+              rejectUnauthorized: false,
+          },
+      }
+    : options;
+
+const pool = new Pool(usuRightOptions);
 
 module.exports = pool;
--- utils/constants.js
+++ utils/constants.js
@@ -4,8 +4,6 @@ const postgressOption = {
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    host: process.env.HOST,
-    port: process.env.PORT,
 };
 
 module.exports = postgressOption;
